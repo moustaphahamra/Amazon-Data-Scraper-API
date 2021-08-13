3813736f413c0cfc28c7639af45336ff36fc2f11
user <elmosquitos03@outlook.com>
2021-08-14 01:31:47 +0300

    updates

--- index.js
+++ index.js
@@ -1,77 +1,81 @@
-const express = require('express');
-const request = require('request-promise');
+const express = require("express");
+const request = require("request-promise");
 
-const PORT = process.env.PORT || 5000;
 const app = express();
+const PORT = process.env.PORT || 5000;
+
+ 
+
+const generateScraperUrl = (apiKey) => `http://api.scraperapi.com?api_key=${apiKey}&autoparse=true`;
 
 app.use(express.json());
 
-const returnScraperApiUrl = (apiKey) => `http://api.scraperapi.com?api_key=${apiKey}&autoparse=true`;
-
-// Welcome route
-app.get('/', async (req, res) => {
-    res.send('Welcome to Amazon Scraper API!');
+app.get("/", (req, res) => {
+  res.send("Welcome to Amazon Scraper API.");
 });
 
-// Get product details
-app.get('/products/:productId', async (req, res) => {
-    const { productId } = req.params;
-    const { api_key } = req.query;
-    //api_key = '1494a940dcb3dc7ba2d094b894b1d6a3';
+//Get product details
+app.get("/products/:productId", async (req, res) => {
+  const { productId } = req.params;
+  const {api_key} = req.query
 
-    try {
-        const response = await request(`${returnScraperApiUrl(api_key)}&url=https://www.amazon.com/dp/${productId}`);
-        
-        res.json(JSON.parse(response));
-    } catch (error) {
-        res.json(error);
-    }
+  try {
+    const response = await request(
+      `${generateScraperUrl(api_key)}&url=https://www.amazon.com/dp/${productId}`
+    );
+
+    res.json(JSON.parse(response));
+  } catch (error) {
+    res.json(error);
+  }
 });
 
-// Get product reviews
-app.get('/products/:productId/reviews', async (req, res) => {
-    const { productId } = req.params;
-    const { api_key } = req.query;
-    //api_key = '1494a940dcb3dc7ba2d094b894b1d6a3';
+//Get product reviews
+app.get("/products/:productId/reviews", async (req, res) => {
+  const { productId } = req.params;
+  const {api_key} = req.query
 
-    try {
-        const response = await request(`${returnScraperApiUrl(api_key)}&url=https://www.amazon.com/product-reviews/${productId}`);
-        
-        res.json(JSON.parse(response));
-    } catch (error) {
-        res.json(error);
-    }
+  try {
+    const response = await request(
+      `${generateScraperUrl(api_key)}&url=https://www.amazon.com/product-reviews/${productId}`
+    );
+
+    res.json(JSON.parse(response));
+  } catch (error) {
+    res.json(error);
+  }
 });
 
-// Get product offers
-app.get('/products/:productId/offers', async (req, res) => {
-    const { productId } = req.params;
-    const { api_key } = req.query;
-    //api_key = '1494a940dcb3dc7ba2d094b894b1d6a3';
+//Get product Offers
+app.get("/products/:productId/offers", async (req, res) => {
+  const { productId } = req.params;
+  const {api_key} = req.query
 
-    try {
-        const response = await request(`${returnScraperApiUrl(api_key)}&url=https://www.amazon.com/gp/offer-listing/${productId}`);
-        
-        res.json(JSON.parse(response));
-    } catch (error) {
-        res.json(error);
-    }
+  try {
+    const response = await request(
+      `${generateScraperUrl(api_key)}&url=https://www.amazon.com/gp/offer-listing/${productId}`
+    );
+
+    res.json(JSON.parse(response));
+  } catch (error) {
+    res.json(error);
+  }
 });
 
-// Get search results
-app.get('/search/:searchQuery', async (req, res) => {
-    const { searchQuery } = req.params;
-    const { api_key } = req.query;
-    
-    //api_key = '1494a940dcb3dc7ba2d094b894b1d6a3';
+//Get Search Results
+app.get("/search/:searchQuery", async (req, res) => {
+  const { searchQuery } = req.params;
+  const {api_key} = req.query
 
-    try {
-        const response = await request(`${returnScraperApiUrl(api_key)}&url=https://www.amazon.com/s?k=${searchQuery}`);
-        
-        res.json(JSON.parse(response));
-    } catch (error) {
-        res.json(error);
-    }
+  try {
+    const response = await request(
+      `${generateScraperUrl(api_key)}&url=https://www.amazon.com/s?k=${searchQuery}`
+    );
+
+    res.json(JSON.parse(response));
+  } catch (error) {
+    res.json(error);
+  }
 });
 
-app.listen(PORT, () => console.log(`Server Running on Port: ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
